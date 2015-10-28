d9c88f60ced27b434d1bcc39f79da41d5194bb4a
Student <student@RU-019.local>
2015-10-28 16:29:19 -0600

    more maps

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,10 @@
-angular.module('wineApp', ['leaflet-directive'])
+angular.module('wineApp', [])
 
 angular.module('wineApp')
 	.controller('wineTroller', ['$scope', '$http', function($scope, $http){
 
 		console.log('Hello')
 
-		L.mapbox.accessToken = 'pk.eyJ1IjoidGxvc3RlbnNvbiIsImEiOiJjaWdheWlseXMxbjJ4d25rbzk2MGppMTZxIn0.HHjHSOxre0no93YdRrVjPA';
-		L.mapbox.map('map-one', 'mapbox.streets');
 		
 
 	}])
---
+++ public/javascripts/map.js
@@ -0,0 +1,2 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoidGxvc3RlbnNvbiIsImEiOiJjaWdheWlseXMxbjJ4d25rbzk2MGppMTZxIn0.HHjHSOxre0no93YdRrVjPA';
+		L.mapbox.map('world', 'mapbox.wine');
